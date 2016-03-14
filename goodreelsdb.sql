4fafe4aa7e604a0c48ca430f72b5538ba187cda4
Rebecca Robb <robbr@colorado.edu>
2016-03-13 18:51:55 -0600

    modified goodreelsdb.sql to import csv

--- goodreelsdb.sql
+++ goodreelsdb.sql
@@ -1,16 +1,15 @@
 create table if not exists `movie` (
 `id` int(1) not null auto_increment,
 `name` varchar(100) not null,
-`release_year`int(1) not null,
+`release_year` int(1) not null,
 `director` varchar(100) not null,
 `mpaa_rating` varchar(5) not null,
 `duration` int(1) not null,
 primary key (`id`)
 ) AUTO_INCREMENT=7;
 
-insert into `movie` (`id`, `name`, `release_year`, `director`, `mpaa_rating`, `duration`) values
-  (1, 'Star Wars', 1977, 'George Lucas', 'PG', 121),
-  (2, 'The Godfather', 1972, 'Francis Ford Coppola', 'R', 175),
-  (3, 'Citizen Kane', 1941, 'Orson Welles', 'N/A', '119'),
-  (4, 'Casablanca', 1942, 'Michael Curtiz', 'PG', 102),
-  (5, 'Raging Bull', 1980, 'Martin Scorsese', 'R', 129);
+load data local infile 'MovieTestDB.csv' into table `movie` fields
+terminated by ','
+enclosed by '"'
+lines terminated by '/n'
+(`id`,`name`, `release_year`, `director`, `mpaa_rating`, `duration`)
